663729ec95b8b97a270ae4133aa75ba489491500
Balbisaur <albi3479@yahoo.com>
2024-04-15 00:17:49 -0500

    first commit

---
+++ The_Range_Riddle.py
@@ -0,0 +1,6 @@
+days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+print(days)
+for index in range(len(days)):
+    print(days[0], days[2], days[4], days[6])
+    break
