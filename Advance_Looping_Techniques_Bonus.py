00e5a15102f56f640b76a005029700e3be3b858a
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-04-15 16:10:07 -0500

    Add files via upload

---
+++ Advance_Looping_Techniques_Bonus.py
@@ -0,0 +1,17 @@
+scoops = ['choclate', 'starwberry', 'vanilla', 'cookie dough', 'brownie']
+
+for scoops_1 in scoops:
+    for scoops_2 in scoops:   
+       if scoops_1 == scoops_2:
+         print(scoops_1, scoops_2)
+
+for scoops_1 in scoops:
+    for scoops_2 in scoops:   
+       if scoops_1 == scoops_2:
+           print('double', scoops_1)
+       else:
+            print(scoops_1, scoops_2)
+
+
+
+
